b54d58f0eb66b2172f3737a156c3bcee452b8fa6
JRumble <jody.paget@rogers.com>
2015-01-23 12:46:01 -0500

    inprogress marsrover.rb

--- marsrover.rb
+++ marsrover.rb
@@ -1,12 +1,14 @@
 #marsrover.rb
+	
 
-
+# There is still two problems with this code that I am aware of 1) it doesn't let the user input a string of instructions 
+#2) I need to use the defaoult settings to reset the co-ordinates for Rover 2
 
 class Rover
 
 	attr_accessor :x, :y, :direction
 
-	def initialize(x, y, direction)
+	def initialize(x = 0, y = 0, direction = "N'")
 		@x = x
 		@y = y
 		@direction = direction
@@ -20,13 +22,16 @@ class Rover
 
 	
 	def read_instruction
-		input = gets.chomp
-		if input.upcase == "M"
-			move
-		else 
-			turn(input)
+		5.times  do input = gets.chomp
+		#end- should end here but breaks the code and don't have time to figure it out why but will on the weekend
+			if input.upcase == "M"
+				move
+			else 
+				turn(input)
+			end
+				puts "You are now at #{self}."
 		end
-			puts "You are now at #{self}."
+		
 	end
 
 	def move
@@ -72,7 +77,7 @@ puts "Your current position is #{rover}."
 		i += 1
 			puts "Enter 'M' to move forward, 'R' to turn right or 'L' to turn left."
 			rover.read_instruction
-		break if i == 5
+		break if i == 2
 	end
 	
 
